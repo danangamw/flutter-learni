3f7ea1436158eabd49ccd9ecbeba6acc0e3eb605
danangamw <danangamw@gmail.com>
2023-05-30 07:46:52 +0700

    update learning part 3 - debugging with flutter

--- 1-roll_dice_app/lib/main.dart
+++ 1-roll_dice_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer(colors: [Colors.purple, Colors.deepPurple]),
