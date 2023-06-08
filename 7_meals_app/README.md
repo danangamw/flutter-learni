5b908fa32ab121d89520c2a31e58a02dded31fc3
Danangamw <danangamw@gmail.com>
2023-06-08 13:16:28 +0700

    update learning section 8

---
+++ 7_meals_app/README.md
@@ -0,0 +1,9 @@
+What i've learned
+
+- Navigation between screens
+- set filter
+- pick item with filter
+- Navigator.push to move to another page || Navigator.pushReplaced to move another page and not add stack to current stack
+- Navigator.pop
+- How to add side drawer
+- How to add side drawer navigation
---
+++ 7_meals_app/lib/screens/tabs.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dumm_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+  Map<Filter, bool> _selectedFilters = kInitialFilters;
+
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    // check if meals already favorited
+    final isExisting = _favoriteMeals.contains(meal);
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favorite');
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as favorite!');
+      });
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+
+    Widget activePage = CategoriesScreen(
+      onToggleFavorites: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
+    );
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorites: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = 'Your Favorites';
+    }
+
+    void _setScreen(String identifier) async {
+      Navigator.pop(context);
+      if (identifier == 'filters') {
+        final result = await Navigator.push<Map<Filter, bool>>(
+          context,
+          MaterialPageRoute(
+            builder: (ctx) => FilterScreen(currentFilters: _selectedFilters),
+          ),
+        );
+
+        setState(() {
+          _selectedFilters = result ?? kInitialFilters;
+        });
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          _selectPage(index);
+        },
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal_outlined),
+            activeIcon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_outlined),
+            activeIcon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
